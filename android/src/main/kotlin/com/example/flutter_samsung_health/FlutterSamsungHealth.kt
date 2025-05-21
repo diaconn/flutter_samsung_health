63569413f3e48dda9585cfd3fc0f62637ba19be3
yoonhongsuk <youn7986@g2e.co.kr>
2025-05-21 13:56:42 +0900

    :zap: 소스정리

--- android/src/main/kotlin/com/example/flutter_samsung_health/FlutterSamsungHealth.kt
+++ android/src/main/kotlin/com/example/flutter_samsung_health/FlutterSamsungHealth.kt
@@ -33,7 +33,6 @@ import java.util.TimeZone
 /** FlutterSamsungHealth */
 class FlutterSamsungHealth: FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
-  ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
