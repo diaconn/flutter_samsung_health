5a23595f3a810be7ba59fc8f6f3d369cee3d8d5a
thinkseung <victorymj0625@gmail.com>
2025-12-09 18:11:57 +0900

    :zap: 수면 데이터 조회 기간 수정(롤백)

--- android/src/main/kotlin/com/example/flutter_samsung_health/FlutterSamsungHealth.kt
+++ android/src/main/kotlin/com/example/flutter_samsung_health/FlutterSamsungHealth.kt
@@ -1291,7 +1291,7 @@ class FlutterSamsungHealth : FlutterPlugin, MethodCallHandler, ActivityAware, St
                     } ?: emptyList<Map<String, Any>>()
 
                     mapOf(
-                        "session_number" to (index + 1),
+                        "session_no" to (index + 1),
                         "session_start_time" to (session.startTime?.toEpochMilli() ?: 0L),
                         "session_end_time" to (session.endTime?.toEpochMilli() ?: 0L),
                         "session_duration" to (session.duration?.toMillis() ?: 0L),
